dcd54941e5b8c92354f6f559cedaaae41d699460
Pablo Shabunio <pshabunia@gmail.com>
2019-06-21 04:18:01 -0700

    Tuned testing tools

---
+++ raspberrypi/servo_control.py
@@ -0,0 +1,8 @@
+import time
+import math
+import random
+from servo import Servo
+
+servo = Servo()
+servo.position(1620,1900)
+
--- raspberrypi/servo_test.py
+++ raspberrypi/servo_test.py
@@ -7,7 +7,9 @@ servo = Servo()
 try:
     while True:
         for x in range (0, 360, random.randrange(5, 20)):
-            rad = math.radians(x) if random.random() > 0.03 else -40 
+            rad = math.radians(x)
+            if random.random() > 0.95:
+                rad = math.radians(x-40)
             servo.position(1600 + math.sin(rad)*300, 1900 + math.cos(rad)*200)
             time.sleep(random.uniform(0.1, 0.8))
 except KeyboardInterrupt:
